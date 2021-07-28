c0d07f570a98a5d3b4421bf511db5f2576be20ea
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-07-28 19:49:38 -0300

    fix

--- desafio8.sql
+++ desafio8.sql
@@ -1,5 +1,5 @@
-SELECT
-  ContactName AS 'Nome de contato',
+SELECT 
+  C.ContactName AS 'Nome de contato', 
   S.ShipperName AS 'Empresa que fez o envio',
   O.OrderDate AS 'Data do pedido'
 FROM w3schools.customers AS C
@@ -7,6 +7,6 @@ INNER JOIN w3schools.orders AS O
   ON C.CustomerID = O.CustomerID
 INNER JOIN w3schools.shippers AS S
   ON S.ShipperID = O.ShipperID
-WHERE S.ShipperName = 'Speedy Express'
-OR 'United Package'
-ORDER BY `Nome de contato`, `Empresa que fez o envio`;
+WHERE S.ShipperName 
+IN('Speedy Express', 'United Package')
+ORDER BY `Nome de contato`, `Empresa que fez o envio`, `Data do pedido`;
