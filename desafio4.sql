d2d8cefe17bb6a482275d21e95d3f76c9c41fa85
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-07-28 16:25:55 -0300

    fix2

--- desafio4.sql
+++ desafio4.sql
@@ -2,7 +2,7 @@ SELECT J.JOB_TITLE AS 'Cargo', ROUND(AVG(E.SALARY),2) AS 'Média salarial',
 CASE
 WHEN AVG(E.SALARY) <= 5800 THEN 'Júnior'
     WHEN AVG(E.SALARY) <= 7500 THEN 'Pleno'
-WHEN AVG(E.SALARY) <= 10500 THEN 'Senior'
+WHEN AVG(E.SALARY) <= 10500 THEN 'Sênior'
     ELSE 'CEO'
 END AS 'Senioridade'
 FROM  hr.jobs AS J
