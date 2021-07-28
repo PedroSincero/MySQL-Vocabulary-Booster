04a1d59d613aa06b4cac30b280d38e2cf5ac5568
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-07-28 16:13:51 -0300

    REQ4 - FInalizando requisito 4 - [DONE]

---
+++ desafio4.sql
@@ -0,0 +1,12 @@
+SELECT J.JOB_TITLE AS 'Cargo', ROUND(AVG(E.SALARY),2) AS 'Média salarial',
+CASE
+	WHEN AVG(E.SALARY) <= 5800 THEN 'Júnior'
+    WHEN AVG(E.SALARY) <= 7500 THEN 'Pleno'
+	WHEN AVG(E.SALARY) <= 10500 THEN 'Senior'
+    ELSE 'CEO'
+END AS 'Senioridade'
+FROM  hr.jobs AS J
+INNER JOIN hr.employees AS E
+ON E.JOB_ID = J.JOB_ID
+GROUP BY  `Cargo`
+ORDER BY `Média salarial`, `Cargo`;
