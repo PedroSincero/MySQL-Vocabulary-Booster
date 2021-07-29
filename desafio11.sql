ee1cdc7dd6a64fc16fa38eb365fbc8a16b40e1c3
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-07-29 12:30:52 -0300

    fix

--- desafio11.sql
+++ desafio11.sql
@@ -3,9 +3,9 @@ SELECT
   C1.Country AS 'País',
   COUNT(C1.Country) AS 'Número de compatriotas'
 FROM w3schools.customers AS C1,
-     w3schools.customers AS C2
+      w3schools.customers AS C2
 WHERE C1.Country = C2.Country
 AND C1.ContactName <> C2.ContactName
 GROUP BY `Nome`,
-         `País`
+          `País`
 ORDER BY `Nome`;
