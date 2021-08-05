9fa68aa72d0a598a970c5df52308e2d4a6901281
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-08-05 16:34:27 -0300

    REQ16 - Finalizando Requisito - [DONE]

---
+++ desafio16.sql
@@ -0,0 +1,12 @@
+DELIMITER $ $ CREATE FUNCTION buscar_quantidade_de_empregos_por_funcionario (param_email VARCHAR(50)) RETURNS INT READS SQL DATA BEGIN DECLARE result INT;
+SELECT
+  COUNT(jh.JOB_ID)
+FROM
+  hr.job_history AS jh
+  INNER JOIN hr.employees AS e ON e.EMAIL = param_email
+WHERE
+  e.EMPLOYEE_ID = jh.EMPLOYEE_ID INTO result;
+RETURN result;
+END $ $ DELIMITER;
+
+-- Agradecimentos a Denis Rossati Turma 10, Tribo B, por ter me auxiliado na elaboração deste requisito e de mais outros
