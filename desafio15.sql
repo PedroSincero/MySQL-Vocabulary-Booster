cf9147af64c6520059c2cb5d0586e434724116c7
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-08-05 15:43:06 -0300

    REQ15 SEILA

--- desafio15.sql
+++ desafio15.sql
@@ -1,10 +1,9 @@
-USE hr;
-DELIMITER $ $ CREATE PROCEDURE buscar_media_por_cargo (IN cargo VARCHAR(80)) BEGIN
+DELIMITER $ $ CREATE PROCEDURE buscar_media_por_cargo (IN param_cargo VARCHAR(50)) BEGIN
 SELECT
   ROUND(AVG(e.SALARY), 2) AS 'Média salarial'
 FROM
   hr.employees AS e
-  INNER JOIN hr.jobs AS j ON j.JOB_ID = cargo
+  INNER JOIN hr.jobs AS j ON j.JOB_TITLE = param_cargo
 WHERE
-  j.JOB_ID = e.JOB_ID;
+  j.JOB_ID = E.JOB_ID;
 END $ $ DELIMITER;
