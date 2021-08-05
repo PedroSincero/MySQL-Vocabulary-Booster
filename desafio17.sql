52df4d73d46acdb5298546ffabf7d16bb5bc483b
Pedro Henrique Pires do Nascimento <dckpedro@gmail.com>
2021-08-05 16:48:47 -0300

    REQ17 - Finalizando Requisito 17 - [DONE]

---
+++ desafio17.sql
@@ -0,0 +1,6 @@
+DELIMITER $ $ CREATE TRIGGER data_atual BEFORE
+INSERT
+  ON w3schools.orders FOR EACH ROW BEGIN
+SET
+  NEW.OrderDate = DATE(NOW())
+END DELIMITER $ $ -- Agradecimentos a Denis Rossati Turma 10, Tribo B, por ter me auxiliado na elaboração deste requisito e de mais outros
